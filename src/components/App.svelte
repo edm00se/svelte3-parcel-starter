3b917d4013f40e15ecfe4e483076de7461bea6f8
Eric McCormick <edm00se@gmail.com>
2019-10-09 09:29:12 -0500

    adds some standard styles
    mimics vue-parcel-starter

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,19 +1,42 @@
-<h1>Hello Svelte3 + Parcel!</h1>
-<div>
-	<ul>
-		<li><a href="https://parceljs.org/" target="_blank" rel="noopener noreferrer">Parcel</a></li>
-		<li><a href="https://svelte.dev/" target="_blank" rel="noopener noreferrer">Svelte</a></li>
-	</ul>
+<div class="wrapper">
+	<div class="content">
+		<h1>Hello Svelte3 + Parcel!</h1>
+		<ul>
+			<li><a href="https://parceljs.org/" target="_blank" rel="noopener noreferrer">Parcel</a></li>
+			<li><a href="https://svelte.dev/" target="_blank" rel="noopener noreferrer">Svelte</a></li>
+		</ul>
+		<p>
+			Hello { name }, the time is <span class="the-time">{ hours }:{ minutes }:{ seconds }</span>
+		</p>
+	</div>
 </div>
 
-<p>
-	Hello { name }, the time is <span class="the-time">{ hours }:{ minutes }:{ seconds }</span>
-</p>
-
 <style>
-  .the-time {
+	.wrapper {
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: center;
+		align-items: center;
+		height: 100%;
+	}
+	.content {
+		text-align: center;
+	}
+	h1 {
+		font-weight: 300;
+	}
+	h2 {
+		font-weight: 500;
+	}
+	a {
+		color: #ff3e00;
+	}
+	.the-time {
     font-weight: bold;
   }
+	ul {
+		list-style: none;
+	}
 </style>
 
 <script>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import '@babel/polyfill';
 
+import './styles.css';
 import App from './components/App.svelte';
 
 const app = new App({
-  target: document.getElementById('app'),
+  target: document.body,
   props: {
     name: 'world'
   }
